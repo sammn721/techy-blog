52a50e72d2a917d9b6945ceb693cea73a4d3e1f1
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-28 01:50:33 -0700

    homepage.handlebars

--- models/Post.js
+++ models/Post.js
@@ -11,7 +11,7 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    date: {
+    date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
